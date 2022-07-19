47c2eaf839fc328976beb6427dd80d4a67c6df11
thiago <thiagooffice2@gmail.com>
2022-07-19 19:42:05 -0300

    feat: using next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,26 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 export default function SignInButton(){
 
-    const isUserLoggedIn = false
+    const {data: session} = useSession()
+    console.log('session :>>', session);
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
         >
             <FaGithub color='#04d361' />
-            Thiago leal
+            {session?.user?.name}
             <FiX color='#737380' className={styles.closeIcon}/>
         </button>
     ) : (
         <button type="button"
         className={styles.signInButton}
+        onClick={() => signIn('github')}
         >
             <FaGithub color='#e6a417' />
             Sign in with Github
